71539090896990cab678b9a7551d6bec1be0c90b
Christopher Truncer <chris@fortynorthsecurity.com>
2021-07-22 14:50:14 -0600

    Added function to enumerate current domain

---
+++ EDD/Functions/GetCurrentDomain.cs
@@ -0,0 +1,17 @@
+﻿using EDD.Models;
+
+using System.DirectoryServices.ActiveDirectory;
+
+namespace EDD.Functions
+{
+    public class GetCurrentDomain : EDDFunction
+    {
+        public override string FunctionName => "GetCurrentDomain";
+
+        public override string[] Execute(ParsedArgs args)
+        {
+            Domain domain = Domain.GetCurrentDomain();
+            return new string[] { domain.Name };
+        }
+    }
+}
