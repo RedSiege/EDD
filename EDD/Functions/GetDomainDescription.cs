0d4554641fc57f93737d6d9b3efe1275fa210e45
Gr1mmie <Bikuta5201@gmail.com>
2022-05-31 14:46:29 -0700

    added description, better implementation coming soon

---
+++ EDD/Functions/GetDomainDescription.cs
@@ -0,0 +1,25 @@
+﻿using EDD.Models;
+
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+namespace EDD.Functions
+{
+    internal class GetDomainDescription : EDDFunction
+    {
+        public override string FunctionName => "Descriptions";
+
+        public override string[] Execute(ParsedArgs args)
+        {
+            List<String> QueryOutList = new List<String>();
+            
+            if(args.ldapQuery == null) { args.ldapQuery = "(&(objectclass=user)(description=*))"; }
+
+            SearchResultCollection QueryOut = LDAP.CustomSearchLDAP($"{args.ldapQuery}");
+            foreach (SearchResult res in QueryOut) { QueryOutList.Add($"{res.Properties["CN"][0]}\t\t{res.Properties["Description"][0]}"); }
+
+            return QueryOutList.ToArray(); 
+        }
+    }
+}
